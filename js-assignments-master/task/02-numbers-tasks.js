84edf98868321e448c571bd45d272d0fb372b706
Artem <TemaCap1992@github.com>
2020-09-22 15:19:39 +0300

    done 2 task of numbers

--- js-assignments-master/task/02-numbers-tasks.js
+++ js-assignments-master/task/02-numbers-tasks.js
@@ -38,7 +38,7 @@ function getRectangleArea(width, height) {
  *   0    => 0
  */
 function getCicleCircumference(radius) {
-    throw new Error('Not implemented');
+    return radius * 2 * Math.PI;
 }
 
 /**
