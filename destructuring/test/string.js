da93d95f7b50c77c1106432f24601cd0ca06bd7d
Artem <TemaCap1992@github.com>
2020-09-28 18:50:24 +0300

    fixed test string

--- destructuring/test/string.js
+++ destructuring/test/string.js
@@ -7,14 +7,17 @@ var assert = require('assert');
 describe('Destructuring also works on strings', () => {
   it('destructure every character, just as if the string was an array', () => {
     let a, b, c = 'abc';
+    return ([a, b, c], ['a', 'b', 'c']);
     assert.deepEqual([a, b, c], ['a', 'b', 'c']);
   });
   it('missing characters are undefined', () => {
     const [a, c] = 'ab';
+    return (c, void 0);
     assert.equal(c, void 0);
   });
   it('unicode character work too', () => {
     const [space, coffee] = 'a ☕';
+    return (coffee, '\u{2615}');
     assert.equal(coffee, '\u{2615}');
   });
 });
